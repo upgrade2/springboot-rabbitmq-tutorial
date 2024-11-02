f0a4b5fa138f780ff9b8294f72e85b7751ffd216
Ajay <ajay.singh.114085@gmail.com>
2024-11-03 00:39:20 +0530

    Producer In RabbitMQ

---
+++ src/main/java/net/javaguides/springboot/config/RabbitMQConfig.java
@@ -0,0 +1,43 @@
+package net.javaguides.springboot.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+    @Value("${rabbitmq.queue.name}")
+    private String queue;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    // spring bean for rabbitmq queue
+    @Bean
+    public Queue queue(){
+        return new Queue(queue);
+    }
+    @Bean
+    public TopicExchange exchange(){
+       return new TopicExchange(exchange);
+    }
+
+    @Bean
+    public Binding binding(){
+        return BindingBuilder.bind(queue())
+                .to(exchange())
+                .with(routingKey);
+    }
+
+    //below will be autoconfigured by spring boot.
+    //ConnectionFactory
+    //RabbitTemplate
+    //RabbitAdmin
+}
